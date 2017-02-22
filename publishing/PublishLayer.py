caa66c756193731f49972554693591572d60832d
Richard Hughes <richardh522@gmail.com>
2017-02-22 09:24:06 -0500

    passing web map; creating mxdx; exporting pdf

--- publishing/PublishLayer.py
+++ publishing/PublishLayer.py
@@ -2,6 +2,16 @@ import arcpy
 import os
 from datetime import datetime
 from arcpy import mp, env
+from arcgisscripting import ExecuteError
+import logging
+
+home_dir = os.path.dirname(os.path.abspath(__file__))
+
+if not os.path.exists(os.path.join(home_dir, 'logs')):
+    os.mkdir(os.path.join(home_dir, 'logs'))
+
+logging.basicConfig(filename=os.path.join(home_dir, 'logs/publish_layers.txt'))
+logger = logging.getLogger(__name__)
 
 
 # determine folder name from the dataset that contains the feature class to be published
@@ -11,89 +21,103 @@ def get_folder_name(lyr):
     return folder
 
 # Test Parameters for Tool
-p = arcpy.mp.ArcGISProject(r'C:\Users\rhughes\Documents\ArcGIS\Projects\RTAA_publishing\RTAA_publishing.aprx')
-m = p.listMaps()[0]
-layer = m.listLayers()[0]
-map_or_layers = layer
+# p = arcpy.mp.ArcGISProject(r'C:\Users\rhughes\Documents\ArcGIS\Projects\RTAA_publishing\RTAA_publishing.aprx')
+# map = 'LayerMap'
+# m = p.listMaps("{}".format(map))[0]
+# group_layer = 'Airspace'
+# inlayer = 'PRIM77'
+# map_layer = m.listLayers(inlayer)[0]
+# lyr_name = map_layer.name
 
-# map = arcpy.GetParameterAsText(0)
-# group_layer = arcpy.GetParameterAsText(1)
-# inlayer = arcpy.GetParameterAsText(2)
-#get layers that are sublayers or individual
-# inlayer = arcpy.Describe(layer).name.split("\\")[-1]
+try:
+    p = mp.ArcGISProject('current')
+    map = arcpy.GetParameterAsText(0)
+    group_layer = arcpy.GetParameterAsText(1)
+    map_layer = arcpy.GetParameter(2)
+    lyr_name = map_layer.name
 
-# p = mp.ArcGISProject('current')
+    logger.warning("lyr_name :: {}".format(lyr_name))
+    m = p.listMaps("{}".format(map))[0]
 
-# output_folder = r"C:\Users\rhughes\Documents\ArcGIS\Projects\RTAA_publishing\scratch"
-output_folder = os.path.join(os.path.dirname(p.filePath), "scratch")
-env.workspace = output_folder
+    leaf_layer = lyr_name.split("\\")[-1]
+    map_layer = m.listLayers(leaf_layer)[0]
 
-m = p.listMaps("{}".format(map))[0]
-arcpy.AddMessage("Map: {}".format(m))
+    arcpy.AddMessage("Map: {}".format(m))
 
-# map_or_layers = m.listLayers("{}".format(arcpy.Describe(layer).name))[0]
-# map_or_layers = m.listLayers("{}".format(layer))[0]
+    output_folder = os.path.join(os.path.dirname(p.filePath), "scratch")
+    env.workspace = output_folder
 
-arcpy.AddMessage("Layer: {}".format(map_or_layers.name))
-arcpy.AddMessage("CatalogPath: {}".format(arcpy.Describe(map_or_layers).catalogPath))
+    arcpy.AddMessage("Tests : {}".format(",".join([x.name for x in m.listLayers() if x.isFeatureLayer])))
 
-out_sddraft = os.path.join(output_folder, "{}.sddraft".format(arcpy.Describe(map_or_layers).name))
-arcpy.AddMessage("out_sddraft: {}".format(out_sddraft))
+    arcpy.AddMessage("groupLayer :: {}".format(group_layer))
 
-service_name = arcpy.Describe(map_or_layers).name.replace("_", " ")
-arcpy.AddMessage("service_name: {}".format(service_name))
+    # arcpy.AddMessage("Layer: {}".format(lyr_name))
+    arcpy.AddMessage("CatalogPath: {}".format(arcpy.Describe(map_layer).catalogPath))
 
-server_type = "MY_HOSTED_SERVICES"
-arcpy.AddMessage("server_type: {}".format(server_type))
+    out_sddraft = os.path.join(output_folder, "{}.sddraft".format(leaf_layer))
+    if os.path.exists(out_sddraft):
+        os.remove(out_sddraft)
+    arcpy.AddMessage("out_sddraft: {}".format(out_sddraft))
 
-service_type = "FEATURE_ACCESS"
-arcpy.AddMessage("service_type: {}".format(service_type))
+    arcpy.AddMessage("service_name: {}".format(leaf_layer))
 
-folder_name = get_folder_name(map_or_layers)
-arcpy.AddMessage("folder_name: {}".format(folder_name))
+    server_type = "MY_HOSTED_SERVICES"
+    arcpy.AddMessage("server_type: {}".format(server_type))
 
-overwrite_existing_service = True
-arcpy.AddMessage("overwrite_existing_service: {}".format(overwrite_existing_service))
+    service_type = "FEATURE_ACCESS"
+    arcpy.AddMessage("service_type: {}".format(service_type))
 
-copy_data_to_server = True
-arcpy.AddMessage("copy_data_to_server: {}".format(copy_data_to_server))
+    folder_name = get_folder_name(map_layer)
+    arcpy.AddMessage("folder_name: {}".format(folder_name))
 
-enable_editing = False
-arcpy.AddMessage("enable_editing: {}".format(enable_editing))
+    overwrite_existing_service = True
+    arcpy.AddMessage("overwrite_existing_service: {}".format(overwrite_existing_service))
 
-allow_exporting = True
-arcpy.AddMessage("allow_exporting: {}".format(allow_exporting))
+    copy_data_to_server = True
+    arcpy.AddMessage("copy_data_to_server: {}".format(copy_data_to_server))
 
-enable_sync = False
-arcpy.AddMessage("enable_sync: {}".format(enable_sync))
+    enable_editing = False
+    arcpy.AddMessage("enable_editing: {}".format(enable_editing))
 
-summary = "{} published on {}".format(service_name, datetime.ctime(datetime.today()))
-arcpy.AddMessage("summary: {}".format(summary))
+    allow_exporting = True
+    arcpy.AddMessage("allow_exporting: {}".format(allow_exporting))
 
-tags = ", ".join([folder_name, service_name])
-arcpy.AddMessage("tags: {}".format(tags))
+    enable_sync = False
+    arcpy.AddMessage("enable_sync: {}".format(enable_sync))
 
-description = group_layer
-arcpy.AddMessage("description: {}".format(description))
+    summary = "{} published on {}".format(lyr_name, datetime.ctime(datetime.today()))
+    arcpy.AddMessage("summary: {}".format(summary))
 
-credits = "RTAA"
-arcpy.AddMessage("credits: {}".format(credits))
+    tags = ", ".join([folder_name, leaf_layer])
+    arcpy.AddMessage("tags: {}".format(tags))
 
-use_limitations = "RTAA"
-arcpy.AddMessage("use_limitations: {}".format(use_limitations))
+    description = group_layer
+    arcpy.AddMessage("description: {}".format(description))
 
-p.save()
-mp.CreateWebLayerSDDraft(map_or_layers=map_or_layers, out_sddraft=out_sddraft, service_name=service_name,
-                         server_type=server_type, service_type=service_type, folder_name=folder_name,
-                         overwrite_existing_service=overwrite_existing_service, copy_data_to_server=copy_data_to_server,
-                         enable_editing=enable_editing, allow_exporting=allow_exporting, enable_sync=enable_sync,
-                         summary=summary, tags=tags, description=description, credits=credits,
-                         use_limitations=use_limitations)
+    credits = "RTAA"
+    arcpy.AddMessage("credits: {}".format(credits))
 
-definition = out_sddraft.replace(".sddraft", ".sd")
-arcpy.StageService_server(out_sddraft, definition)
-arcpy.UploadServiceDefinition_server(definition, 'My Hosted Services')
+    use_limitations = "RTAA"
+    arcpy.AddMessage("use_limitations: {}".format(use_limitations))
 
+    p.save()
+    mp.CreateWebLayerSDDraft(map_or_layers=map_layer, out_sddraft=out_sddraft, service_name=leaf_layer,
+                             server_type=server_type, service_type=service_type, folder_name=folder_name,
+                             overwrite_existing_service=overwrite_existing_service, copy_data_to_server=copy_data_to_server,
+                             enable_editing=enable_editing, allow_exporting=allow_exporting, enable_sync=enable_sync,
+                             summary=summary, tags=tags, description=description, credits=credits,
+                             use_limitations=use_limitations)
+
+    definition = out_sddraft.replace(".sddraft", ".sd")
+    if os.path.exists(definition):
+        os.remove(definition)
+
+    arcpy.StageService_server(out_sddraft, definition)
+
+    arcpy.UploadServiceDefinition_server(definition, 'My Hosted Services')
+except ExecuteError as e:
+    print(e)
+    pass
 
 
 
