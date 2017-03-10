1b1f7c63a55d7e766c85bfaf58057ebcafd127e3
Richard Hughes <richardh522@gmail.com>
2017-03-10 15:14:54 -0500

    update to print tool

--- repairMapLayers.py
+++ repairMapLayers.py
@@ -1,14 +1,15 @@
 import arcpy
 from arcpy import mp
 import argparse
+import os
 
 
 class LayerRepairTool:
-    def __init__(self, project):
-        self.aprx = mp.ArcGISProject(project)
+    def __init__(self, project_path):
+        self.aprx = mp.ArcGISProject(project_path)
 
     def repair(self, target_gdb):
-        m = self.aprx.listMaps('Map')[0]
+        m = self.aprx.listMaps('LayerMap')[0]
         lyrs = [x for x in m.listLayers() if x.isFeatureLayer]
         for l in lyrs:
             try:
@@ -32,10 +33,16 @@ if __name__ == "__main__":
         project = args.project
     else:
         project = r"C:\Users\rhughes\Documents\ArcGIS\Projects\RTAA_Printing\RTAA_Printing.aprx"
+        if not os.path.exists(project):
+            project = r"C:\inetpub\rtaa_gis_data\RTAA_Printing\RTAA_Printing.aprx"
 
     if args.gdb is not None:
         gdb = args.gdb
     else:
         gdb = r"C:\ESRI_WORK_FOLDER\rtaa\MasterGDB\MasterGDB_05_25_16\MasterGDB_05_25_16.gdb"
+        if not os.path.exists(gdb):
+            gdb = r"C:\inetpub\rtaa_gis_data\MasterGDB_05_25_16\MasterGDB_05_25_16.gdb"
+
     rp = LayerRepairTool(project_path=project)
     rp.repair(target_gdb=gdb)
+
