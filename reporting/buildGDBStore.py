a1c225bc199d7a9ef877ea6ad8cc532fbd4a8d79
Richard Hughes <richardh522@gmail.com>
2017-11-11 17:09:00 -0500

    completed models / beginning tests

--- reporting/buildGDBStore.py
+++ reporting/buildGDBStore.py
@@ -2,10 +2,11 @@ import arcpy
 from arcpy import env
 import json
 import os
-import xlwt
+from collections import Counter
 
 
 class DescribeGDB:
+    """Gathers details about a geodatabase"""
     def __init__(self, workspace):
         self.workspace = workspace
         self.desc = arcpy.Describe(workspace)
@@ -16,71 +17,176 @@ class DescribeGDB:
         _desc = self.desc
         context = dict()
         context["baseName"] = _desc.baseName
-        context["workspaceType"] = _desc.workspaceType
         context["catalogPath"] = _desc.catalogPath
+        context["workspaceType"] = _desc.workspaceType
         context["workspaceFactoryProgID"] = _desc.workspaceFactoryProgID
         context["release"] = _desc.release
         context["domains"] = _desc.domains
         context["currentRelease"] = _desc.currentRelease
         context["connectionString"] = _desc.connectionString
         datasets = arcpy.ListDatasets()
-        context["datasets"] = []
-        try:
-            if len(datasets):
-                for dataset in datasets:
-                    props = dict()
-                    env.workspace = "{}\\{}".format(self.workspace, dataset)
-                    descdata = arcpy.Describe(env.workspace)
-                    props["baseName"] = descdata.baseName
-                    props["changeTracked"] = descdata.changeTracked
-                    props["datasetType"] = descdata.datasetType
-                    props["isVersioned"] = descdata.isVersioned
-                    props["children"] = ", ".join([d.name for d in descdata.children])
-
-                    props["spatialReference.XYResolution"] = descdata.spatialReference.XYResolution
-                    props["spatialReference.ZResolution"] = descdata.spatialReference.ZResolution
-                    props["spatialReference.domain"] = descdata.spatialReference.domain
-                    props["spatialReference.factoryCode"] = descdata.spatialReference.factoryCode
-                    props["spatialReference.name"] = descdata.spatialReference.name
-                    props["spatialReference.PCSName"] = descdata.spatialReference.PCSName
-                    props["spatialReference.PCSCode"] = descdata.spatialReference.PCSCode
-                    props["spatialReference.GCSCode"] = descdata.spatialReference.GCSCode
-                    props["spatialReference.GCSName"] = descdata.spatialReference.GCSName
-
-                    fclist = arcpy.ListFeatureClasses()
-                    props["feature_classes"] = []
-
-                    if len(fclist) > 0:
-                        fclist.sort()
-                        for fc in fclist:
-                            count = 0
-                            with arcpy.da.SearchCursor(fc, "*") as cursor:
-                                for row in cursor:
-                                    count += 1
-
-                            if count:
-                                sub_props = dict()
-                                descfc = arcpy.Describe(fc)
-                                sub_props["catalogPath"] = descfc.catalogPath
-                                sub_props["baseName"] = descfc.baseName
-                                sub_props["count"] = count
-                                sub_props["featureType"] = descfc.featureType
-                                sub_props["hasM"] = descfc.hasM
-                                sub_props["hasZ"] = descfc.hasZ
-                                sub_props["hasSpatialIndex"] = descfc.hasSpatialIndex
-                                sub_props["shapeFieldName"] = descfc.shapeFieldName
-                                sub_props["shapeType"] = descfc.shapeType
-                                props["feature_classes"].append(sub_props)
-
-                    if len(props["feature_classes"]):
-                        context["datasets"].append(props)
-
-        except Exception as e:
-            print("{}".format(e))
+        context["datasets"] = datasets
 
         return json.dumps(context, sort_keys=True, separators=(',', ':'), indent=4)
 
+
+class DescribeFDataset:
+    """Gathers details about a Feature Dataset"""
+    def __init__(self, workspace, dataset):
+        self.workspace = os.path.join(workspace, dataset)
+        self.desc = arcpy.Describe(self.workspace)
+
+    def describe(self):
+        env.workspace = self.workspace
+        descdata = self.desc
+        props = dict()
+        props["baseName"] = descdata.baseName
+        props["changeTracked"] = descdata.changeTracked
+        props["datasetType"] = descdata.datasetType
+        props["isVersioned"] = descdata.isVersioned
+        props["spatialReference"] = descdata.spatialReference.factoryCode
+        props["XYResolution"] = descdata.spatialReference.XYResolution
+        props["ZResolution"] = descdata.spatialReference.ZResolution
+        props["PCSName"] = descdata.spatialReference.PCSName
+        props["PCSCode"] = descdata.spatialReference.PCSCode
+        props["GCSCode"] = descdata.spatialReference.GCSCode
+        props["GCSName"] = descdata.spatialReference.GCSName
+
+        fclist = arcpy.ListFeatureClasses()
+        props["children"] = fclist
+
+        return json.dumps(props, sort_keys=True, separators=(',', ':'), indent=4)
+
+
+class DescribeFClass:
+    """Gathers information about a Feature Class"""
+    def __init__(self, workspace, dataset, fclass):
+        self.workspace = os.path.join(workspace, dataset)
+        self.fclass = fclass
+
+    def describe(self):
+        env.worksace = self.workspace
+        fc = self.fclass
+
+        count = 0
+        with arcpy.da.SearchCursor(fc, "*") as cursor:
+            for row in cursor:
+                count += 1
+
+        props = dict()
+        descfc = arcpy.Describe(fc)
+        props["catalogPath"] = descfc.catalogPath
+        props["baseName"] = descfc.baseName
+        props["count"] = count
+        props["featureType"] = descfc.featureType
+        props["hasM"] = descfc.hasM
+        props["hasZ"] = descfc.hasZ
+        props["hasSpatialIndex"] = descfc.hasSpatialIndex
+        props["shapeFieldName"] = descfc.shapeFieldName
+        props["shapeType"] = descfc.shapeType
+        fields = [f.name for f in arcpy.ListFields(fc)]
+        props["fields"] = fields
+
+        return json.dumps(props, sort_keys=True, separators=(',', ':'), indent=4)
+
+
+class DescribeField:
+
+    def __init__(self, _workspace, _dataset, _fc, _field):
+        self.workspace = os.path.join(_workspace, _dataset)
+        self.fc = _fc
+        self.field = _field
+
+    def describe(self):
+        env.workspace = self.workspace
+        infc = self.fc
+        in_field = self.field
+        field = arcpy.ListFields(infc, "{}".format(in_field))[0]
+        props = dict()
+
+        props["name"] = field.name
+        props["aliasName"] = field.aliasName
+        props["baseName"] = field.baseName
+        props["defaultValue"] = field.defaultValue
+        props["domain"] = field.domain
+        props["editable"] = field.editable
+        props["isNullable"] = field.isNullable
+        props["length"] = field.length
+        props["precision"] = field.precision
+        props["required"] = field.required
+        props["scale"] = field.scale
+        props["type"] = field.type
+        system_field_types = ["BLOB", "GEOMETRY", "OID", "GLOBALID"]
+        system_field_names = ["SHAPE_LENGTH", "SHAPE_AREA", "SHAPE", "OBJECTID", "GLOBALID"]
+        editor_field_names = ["CREATED_USER", "CREATED_DATE", "LAST_EDITED_USER", "LAST_EDITED_DATE"]
+        system_field_names.extend(editor_field_names)
+        props["attributes"] = dict()
+        props["percent"] = ""
+
+        if field.type.upper() not in system_field_types and field.name not in system_field_names:
+            attributes = []
+            cnt = 0
+            with arcpy.da.SearchCursor(infc, in_field) as cursor:
+                for row in cursor:
+                    if row[0] is None:
+                        d = "None"
+                    else:
+                        try:
+                            d = "{}".format(row[0])
+                        except UnicodeEncodeError:
+                            d = row[0].encode('ascii', 'ignore')
+                    attributes.append(d)
+                    cnt += 1
+            if cnt:
+                c = Counter(attributes)
+                counts = c.items()
+                for x in counts:
+                    props["attributes"][x[0]] = x[1]
+
+                del c
+
+                i = 0
+                for value in attributes:
+                    try:
+                        d = float(value)
+                        d = int(d)
+                    except ValueError:
+                        d = value
+
+                    if d not in ['', ' ', '0', 'None', '<Null>']:
+                        i += 1
+                    del d
+
+                percent = float(i) / cnt * 100.00
+                props["percent"] = "%.2f" % percent
+
+            else:
+                props["percent"] = '0.0'
+
+        return json.dumps(props, sort_keys=True, separators=(',', ':'), indent=4)
+
+
 if __name__ == "__main__":
-    x = DescribeGDB(r"C:\ESRI_WORK_FOLDER\rtaa\MasterGDB_05_25_16\RTAA_Delivery_05_25_16.gdb")
-    desc = x.describe()
-    print(desc)
+    gdb = r"C:\ESRI_WORK_FOLDER\rtaa\MasterGDB\MasterGDB_07_27_17.gdb"
+    x = DescribeGDB(gdb)
+    gdb_output = x.describe()
+    print(gdb_output)
+
+    datasets = json.loads(gdb_output)["datasets"]
+    for dataset in datasets:
+        desc_dataset = DescribeFDataset(gdb, dataset)
+        dset_output = desc_dataset.describe()
+        print(dset_output)
+
+        fclist = json.loads(dset_output)["children"]
+        for fc in fclist:
+            obj = DescribeFClass(gdb, dataset, fc)
+            fc_out = obj.describe()
+            print(fc_out)
+
+            fields = [f.name for f in arcpy.ListFields(fc)]
+            for field in fields:
+                f_obj = DescribeField(gdb, dataset, fc, field)
+                f_out = f_obj.describe()
+                print(f_out)
+
